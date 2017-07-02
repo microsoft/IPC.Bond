4747ccf7de5112a5a24d947fd548fa675404f4b1
Ara Ayvazyan <aivazyan@microsoft.com>
2017-07-01 17:41:34 -0700

    Fixing broken release build

--- bond.cmd
+++ bond.cmd
@@ -1,18 +1,18 @@
 @setlocal
 
+@if "%1"=="" (set config=Debug) else (set config=%1)
+
 @set PreferredToolArchitecture=x64
 @set BOOST_ROOT=%CD%\IPC\packages\boost.1.63.0.0\lib\native\include
 @set BOOST_LIBRARYDIR=%CD%\IPC\packages\boost.1.63.0.0
 @set BOND_GBC_PATH=%CD%\IPC\packages\Bond.Compiler.6.0.0\tools
 
-@mkdir bond\build
-@mkdir bond\build\target
-
+@mkdir bond\build\target\%config%
 @pushd bond\build
 
-@cmake -G "Visual Studio 14 2015 Win64" -DBOND_LIBRARIES_ONLY=ON -DBOND_ENABLE_COMM=FALSE -DBOND_ENABLE_GRPC=FALSE -DCMAKE_INSTALL_PREFIX=%CD%\target ..
-@cmake --build . --target
-@cmake --build . --target INSTALL
+@cmake -G "Visual Studio 14 2015 Win64" -DBOND_LIBRARIES_ONLY=ON -DBOND_ENABLE_COMM=FALSE -DBOND_ENABLE_GRPC=FALSE -DCMAKE_INSTALL_PREFIX=%CD%\target\%config% ..
+@cmake --build . --config %config% --target
+@cmake --build . --config %config% --target INSTALL
 
 @popd
 @endlocal
