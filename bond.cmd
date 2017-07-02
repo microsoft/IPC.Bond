c0a2a15de03eb9410288dff0483aca6dcc163279
Ara Ayvazyan <ara-ayvazyan@users.noreply.github.com>
2017-07-01 19:57:53 -0700

    Adding CI for Windows (#7)

--- bond.cmd
+++ bond.cmd
@@ -1,6 +1,7 @@
 @setlocal
 
 @if "%1"=="" (set config=Debug) else (set config=%1)
+@if "%~2"=="" (set cmake_args=) else (set cmake_args=-- /logger:%2)
 
 @set PreferredToolArchitecture=x64
 @set BOOST_ROOT=%CD%\IPC\packages\boost.1.63.0.0\lib\native\include
@@ -11,8 +12,8 @@
 @pushd bond\build
 
 @cmake -G "Visual Studio 14 2015 Win64" -DBOND_LIBRARIES_ONLY=ON -DBOND_ENABLE_COMM=FALSE -DBOND_ENABLE_GRPC=FALSE -DCMAKE_INSTALL_PREFIX=%CD%\target\%config% ..
-@cmake --build . --config %config% --target
-@cmake --build . --config %config% --target INSTALL
+@cmake --build . --config %config% --target %cmake_args%
+@cmake --build . --config %config% --target INSTALL %cmake_args%
 
 @popd
 @endlocal
