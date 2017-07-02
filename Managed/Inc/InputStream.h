15fd1ac907fbd2c6bd3301dfdd722a8e04304c0c
Ara Ayvazyan <ara-ayvazyan@users.noreply.github.com>
2017-07-01 17:24:28 -0700

    Fixing tabs (#5)

--- Managed/Inc/InputStream.h
+++ Managed/Inc/InputStream.h
@@ -11,7 +11,7 @@ namespace Bond
 {
 namespace Managed
 {
-	public ref class InputStream : public ::Bond::IO::IInputStream, public ::Bond::IO::ICloneable<InputStream^>
+    public ref class InputStream : public ::Bond::IO::IInputStream, public ::Bond::IO::ICloneable<InputStream^>
     {
     public:
         InputStream(BufferPool::ConstBuffer^ buffer, IPC::Managed::SharedMemory^ inputMemory);
@@ -25,34 +25,34 @@ namespace Managed
         {
             System::Int64 get();
 
-			void set(System::Int64 value);
+            void set(System::Int64 value);
         }
 
-		virtual System::Single ReadFloat();
+        virtual System::Single ReadFloat();
 
-		virtual System::Double ReadDouble();
+        virtual System::Double ReadDouble();
 
-		virtual System::Byte ReadUInt8();
+        virtual System::Byte ReadUInt8();
 
-		virtual System::UInt16 ReadUInt16();
+        virtual System::UInt16 ReadUInt16();
 
-		virtual System::UInt32 ReadUInt32();
+        virtual System::UInt32 ReadUInt32();
 
-		virtual System::UInt64 ReadUInt64();
+        virtual System::UInt64 ReadUInt64();
 
-		virtual System::UInt16 ReadVarUInt16();
+        virtual System::UInt16 ReadVarUInt16();
 
-		virtual System::UInt32 ReadVarUInt32();
+        virtual System::UInt32 ReadVarUInt32();
 
-		virtual System::UInt64 ReadVarUInt64();
+        virtual System::UInt64 ReadVarUInt64();
 
-		virtual System::String^ ReadString(System::Text::Encoding^ encoding, System::Int32 count);
+        virtual System::String^ ReadString(System::Text::Encoding^ encoding, System::Int32 count);
 
-		virtual System::ArraySegment<System::Byte> ReadBytes(System::Int32 count);
+        virtual System::ArraySegment<System::Byte> ReadBytes(System::Int32 count);
 
-		virtual void SkipBytes(System::Int32 count);
+        virtual void SkipBytes(System::Int32 count);
 
-		virtual InputStream^ Clone();
+        virtual InputStream^ Clone();
 
     private:
         InputStream(InputStream% other);
