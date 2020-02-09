9250fde49211e35b8d8808def7990ef646269470
Ara Ayvazyan <ara-ayvazyan@users.noreply.github.com>
2020-02-09 02:12:04 -0800

    Upgrade to vc142 and Boost 1.71 (#12)

--- bond.cmd
+++ bond.cmd
@@ -4,14 +4,14 @@
 @if "%~2"=="" (set cmake_args=) else (set cmake_args=-- /logger:%2)
 
 @set PreferredToolArchitecture=x64
-@set BOOST_ROOT=%CD%\IPC\packages\boost.1.63.0.0\lib\native\include
-@set BOOST_LIBRARYDIR=%CD%\IPC\packages\boost.1.63.0.0
+@set BOOST_ROOT=%CD%\IPC\packages\boost.1.71.0.0\lib\native\include
+@set BOOST_LIBRARYDIR=%CD%\IPC\packages\boost.1.71.0.0
 @set BOND_GBC_PATH=%CD%\IPC\packages\Bond.Compiler.6.0.0\tools
 
 @mkdir bond\build\target\%config%
 @pushd bond\build
 
-@cmake -G "Visual Studio 14 2015 Win64" -DBOND_LIBRARIES_ONLY=ON -DBOND_ENABLE_COMM=FALSE -DBOND_ENABLE_GRPC=FALSE -DCMAKE_INSTALL_PREFIX=%CD%\target\%config% ..
+@cmake -G "Visual Studio 16 2019" -A x64 -DBOND_LIBRARIES_ONLY=ON -DBOND_ENABLE_COMM=FALSE -DBOND_ENABLE_GRPC=FALSE -DCMAKE_INSTALL_PREFIX=%CD%\target\%config% ..
 @cmake --build . --config %config% --target %cmake_args%
 @cmake --build . --config %config% --target INSTALL %cmake_args%
 
