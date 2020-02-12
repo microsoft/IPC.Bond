cea5ebac796a92f61e26adcaf279959918b5954c
Ara Ayvazyan <ara-ayvazyan@users.noreply.github.com>
2020-02-11 19:12:54 -0800

    Upgrade to Bond 8.2.0 (#16)

--- bond.cmd
+++ bond.cmd
@@ -6,12 +6,12 @@
 @set PreferredToolArchitecture=x64
 @set BOOST_ROOT=%CD%\IPC\packages\boost.1.71.0.0\lib\native\include
 @set BOOST_LIBRARYDIR=%CD%\IPC\packages\boost.1.71.0.0
-@set BOND_GBC_PATH=%CD%\IPC\packages\Bond.Compiler.6.0.0\tools
+@set BOND_GBC_PATH=%CD%\IPC\packages\Bond.Compiler.8.2.0\tools
 
 @mkdir bond\build\target\%config%
 @pushd bond\build
 
-@cmake -G "Visual Studio 16 2019" -A x64 -DBOND_LIBRARIES_ONLY=ON -DBOND_ENABLE_COMM=FALSE -DBOND_ENABLE_GRPC=FALSE -DCMAKE_INSTALL_PREFIX=%CD%\target\%config% ..
+@cmake -G "Visual Studio 16 2019" -A x64 -DBOND_LIBRARIES_ONLY=ON -DBOND_ENABLE_GRPC=FALSE -DCMAKE_INSTALL_PREFIX=%CD%\target\%config% ..
 @cmake --build . --config %config% --target %cmake_args%
 @cmake --build . --config %config% --target INSTALL %cmake_args%
 
